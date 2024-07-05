2b02814518b351140ae52e60d5cd68c4ebdb482a
thanhchau <chaun6@gmail.com>
2024-07-05 14:50:50 +0700

    Add New Project Setup SCE
    Add New Project Setup SCE

---
+++ SettupSCE/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SettupSCE
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button2 = new Button();
+            progressBar1 = new ProgressBar();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.AutoSize = true;
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.Location = new Point(-2, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(0, 0);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ActiveCaption;
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(progressBar1);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(-3, 1);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(775, 394);
+            panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.IndianRed;
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(316, 337);
+            button2.Name = "button2";
+            button2.Size = new Size(128, 34);
+            button2.TabIndex = 3;
+            button2.Text = "Finish";
+            button2.UseVisualStyleBackColor = false;
+            button2.Visible = false;
+            button2.Click += button2_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(90, 292);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(621, 23);
+            progressBar1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.YellowGreen;
+            button1.Font = new Font("Verdana", 19F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(262, 172);
+            button1.Name = "button1";
+            button1.Size = new Size(252, 83);
+            button1.TabIndex = 1;
+            button1.Text = "CÀI ĐẶT CHƯƠNG TRÌNH";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(216, 32);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(332, 134);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(772, 396);
+            ControlBox = false;
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Cài Đặt Chương Trình Theo Dõi Máy Soi";
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private Button button2;
+    }
+}
---
+++ SettupSCE/Form1.cs
@@ -0,0 +1,33 @@
+namespace SettupSCE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value <= 80)
+            {
+                progressBar1.Value = progressBar1.Value + 20;
+            }
+            if (progressBar1.Value == 100)
+            {
+                button2.Visible = true;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
